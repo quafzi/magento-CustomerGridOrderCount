41271af7721e4991f78eb0e7923bd3d4ae3e9ba9
Thomas Birke <quafzi@netextreme.de>
2014-12-11 14:04:53 +0100

    Add order count to order grid as well

--- app/code/community/Quafzi/CustomerGridOrderCount/Model/Observer.php
+++ app/code/community/Quafzi/CustomerGridOrderCount/Model/Observer.php
@@ -10,18 +10,22 @@ class Quafzi_CustomerGridOrderCount_Model_Observer
 {
     public function beforeBlockToHtml(Varien_Event_Observer $observer) {
         $block = $observer->getEvent()->getBlock();
-        if ($block instanceof Mage_Adminhtml_Block_Customer_Grid) {
-            $this->_modifyCustomerGrid($block);
+        if ($block instanceof Mage_Adminhtml_Block_Customer_Grid
+            || $block instanceof Mage_Adminhtml_Block_Sales_Order_Grid
+        ) {
+            $after = ($block instanceof Mage_Adminhtml_Block_Customer_Grid)
+                ? 'customer_since'
+                : 'created_at';
+            $this->_modifyGrid($block, $after);
         }
     }
 
-    protected function _modifyCustomerGrid(Mage_Adminhtml_Block_Customer_Grid $grid)
+    protected function _modifyGrid(Mage_Adminhtml_Block_Widget_Grid $grid, $after='customer_since')
     {
-        $this->_addOrderCountColumn($grid);
-
-        // reinitialisiert die Spaltensortierung
+        $this->_addOrderCountColumn($grid, $after);
+        // reinitialize column order
         $grid->sortColumnsByOrder();
-        // reinitialisiert die Sortierung und Filter der Collection
+        // reinitialize collection sort and filter
         $this->_callProtectedMethod($grid, '_prepareCollection');
     }
 
@@ -37,7 +41,7 @@ class Quafzi_CustomerGridOrderCount_Model_Observer
         return $method->invoke($object);
     }
 
-    protected function _addOrderCountColumn($grid)
+    protected function _addOrderCountColumn($grid, $after='customer_since')
     {
         $grid->addColumnAfter('order_count', array(
             'header'    => Mage::helper('customer')->__('Order Count'),
@@ -46,30 +50,45 @@ class Quafzi_CustomerGridOrderCount_Model_Observer
             'type'      => 'number',
             'filter'    => false,
             'index'     => 'order_count'
-        ), 'customer_since');
+        ), $after);
     }
 
-    public function beforeCustomerCollectionLoad(Varien_Event_Observer $observer)
+    public function beforeCollectionLoad($observer)
     {
         $collection = $observer->getEvent()->getCollection();
-        if ($collection instanceof Mage_Customer_Model_Resource_Customer_Collection) {
+        if ($collection instanceof Mage_Customer_Model_Resource_Customer_Collection
+            || $collection instanceof Mage_Sales_Model_Resource_Order_Grid_Collection
+        ) {
             $relationAlias = 'orders_to_count';
-
             $from = $collection->getSelect()->getPart(Zend_Db_Select::FROM);
             if (false === array_key_exists($relationAlias, $from)) {
-                // not yet joined
-                $orderTableName = Mage::getSingleton('core/resource')
-                    ->getTableName('sales/order');
-
-                $collection
-                    ->getSelect()
-                    ->joinLeft(
-                        array($relationAlias => $orderTableName),
-                        $relationAlias . '.customer_id=e.entity_id',
-                        array('order_count' => 'COUNT(' . $relationAlias . '.customer_id)')
-                    );
-                $collection->groupByAttribute('entity_id');
+                $this->_joinOrderCount($collection, $relationAlias);
             }
+        }
+    }
+
+    protected function _joinOrderCount($collection, $relationAlias)
+    {
+        $groupByAttribute = ($collection instanceof Mage_Customer_Model_Resource_Customer_Collection)
+            ? 'entity_id'
+            : 'customer_id';
+        $tableAlias = ($collection instanceof Mage_Customer_Model_Resource_Customer_Collection)
+            ? 'e'
+            : 'main_table';
+        $orderTableName = Mage::getSingleton('core/resource')
+            ->getTableName('sales/order');
+
+        $collection
+            ->getSelect()
+            ->joinLeft(
+                array($relationAlias => $orderTableName),
+                $relationAlias . '.customer_id=' . $tableAlias . '.' . $groupByAttribute,
+                array('order_count' => 'COUNT(' . $relationAlias . '.customer_id)')
+            );
+        if ($collection instanceof Mage_Eav_Model_Entity_Collection_Abstract) {
+            $collection->groupByAttribute($groupByAttribute);
+        } else {
+            $collection->getSelect()->group(array($groupByAttribute));
         }
     }
 }
